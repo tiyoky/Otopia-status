bafffebd9ad1ae6d6e8ba5708e2aa612b37db81c
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-05-14 17:38:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+
+    setInterval(() => {
+        client.user.setPresence({
+            activities: [{
+                name: "otopia soon…",
+                type: "STREAMING",
+                url: "https://discord.gg/hufWHfhnfR"
+            }]
+        });
+    }, 1000);
+});
+
+client.login('process.env.TOKEN');
